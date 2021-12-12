377c3f11fcf69b05c07202f34f38928b80c0315f
FarNys <faridsalmaniy@gmail.com>
2021-12-12 16:06:03 -0700

    change frontend to clien

--- server.js
+++ server.js
@@ -26,9 +26,9 @@ app.use("/sdag87a6fah3ijhwr8faywh/admin", require("./routes/admin"));
 
 //serve static asset
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("/frontend/build"));
+  app.use(express.static("/client/build"));
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
   );
 }
 
